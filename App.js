629d8612d3e8a5ae84d1416ef13c8b4de6a92079
Mathih13 <mathih13@uia.no>
2018-02-09 22:44:56 +0100

    Added firebase config

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firebase from './src/firebase'
+
 export default class App extends React.Component {
   render() {
     return (
