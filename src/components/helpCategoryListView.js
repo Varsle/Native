2e0a38163ce49c85ef939d4eb5c545ad4a154121
Mathih13 <mathih13@uia.no>
2018-02-10 09:06:19 +0100

    Firebase examples in helpcat

--- src/components/helpCategoryListView.js
+++ src/components/helpCategoryListView.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firebase from '../firebase'
 
-export default class helpCategoryListView extends Component {
+export default class HelpCategoryListView extends React.Component {
+
+  componentWillMount() {
+    firebase.database().ref('/categories/').once('value')
+      .then((snap) => {
+        var categories = firebase.toArray(snap)
+        console.log(categories)
+      })
+
+
+      // Code for pushing a new object to db
+      // Push generates a new unique ID
+      firebase.database().ref().child('issues').push().set({
+        body: 'Noen har bæsja på gata',
+        location: {lat: 123123, lon: 1231312},
+        phoneNumber: 12345678
+      })
+  }
 
   render() {
     return(
-
+      <View />
     )
   }
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
+import HelpCategoryListView from '../components/HelpCategoryListView'
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        <HelpCategoryListView />
       </View>
     );
   }
